65e0c603ab6feafa565495dfa51a4000ca69616b
Emin Öztürk <eminozturk963@gmail.com>
2023-06-16 22:31:07 +0300

    İletişim güncellendi

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Contact = () => {
     return (
-        <div className="w-full h-full bg-bg1 px-24 py-10">
+        <div className="w-full h-full bg-bg1 px-24 py-10 flex flex-col justify-center items-center">
             <span className="flex flex-1 justify-center text-white text-3xl font-bold mb-10">İLETİŞİM</span>
-            <div className="text-slate-500 flex justify-center">
-                emin-ozturk78@hotmail.com
+            <div className="w-1/4 flex flex-col bg-bg2 p-4 rounded-md items-center">
+                <span className="font-bold text-white">emin-ozturk78@hotmail.com</span>
             </div>
         </div>
     )
