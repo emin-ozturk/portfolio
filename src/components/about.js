151b17ddf66dab2faaf2df5b69c8870e3d228293
Emin Öztürk <eminozturk963@gmail.com>
2023-06-16 21:42:40 +0300

    Hakkımda güncellendi

--- src/components/about.js
+++ src/components/about.js
@@ -1,5 +1,8 @@
 import React from "react";
-
+import { AiFillAndroid } from "react-icons/ai";
+import { BiAnalyse } from "react-icons/bi";
+import { FaGooglePlay } from 'react-icons/fa';
+import { FiCode } from 'react-icons/fi';
 const About = () => {
     return (
         <div className="w-full h-full bg-bg1 px-24 py-10">
@@ -8,6 +11,31 @@ const About = () => {
                 Node.js, React.js ve MongoDB kullanarak web tabanlı portallar geliştirdim. Bu portallar, firmaların ihtiyaçlarını karşılamak için özelleştirilmiş çözümler sunmaktadır.
                 Java programlama dilini kullanarak Android işletim sistemi için mobil uygulamalar geliştirdim. Node.js ile hazırladığım API'ları bu uygulamalara entegre ederek, firmaların ihtiyaçlarına uygun çözümler sundum. Kullanıcıların ihtiyaçlarını karşılamak için uygulamaların performansı, tasarımı ve kullanılabilirliği gibi konulara özel önem verdim.
                 Ayrıca, temel seviyede Python bilgisi kullanarak makine öğrenmesi alanında çalışmalar yaptım. Bu çalışmalar, veri analizi ve tahmin modelleri oluşturma gibi konuları içeriyordu. Python ve makine öğrenmesi konusunda deneyim kazanarak teknik becerilerimi geliştirdim.
+            </div>
+            <div className="flex flex-row justify-center mt-5">
+                <div className="w-1/5 h-fit bg-bg2 flex flex-col justify-center items-center m-3 p-3">
+                    <FiCode className="w-1/5 h-fit text-white m-3"/>
+                    <span className="font-bold text-white mb-1">Web</span>
+                    <span className="text-slate-500">8 Proje</span>
+                </div>
+
+                <div className="w-1/5 h-fit bg-bg2 flex flex-col justify-center items-center m-3 p-3">
+                    <AiFillAndroid className="w-1/5 h-fit text-white m-3"/>
+                    <span className="font-bold text-white mb-1">Android</span>
+                    <span className="text-slate-500">6 Proje</span>
+                </div>
+
+                <div className="w-1/5 h-fit bg-bg2 flex flex-col justify-center items-center m-3 p-3">
+                    <BiAnalyse className="w-1/5 h-fit text-white m-3"/>
+                    <span className="font-bold text-white mb-1">Makine Öğrenmesi</span>
+                    <span className="text-slate-500">4 Proje</span>
+                </div>
+                
+                <div className="w-1/5 h-fit bg-bg2 flex flex-col justify-center items-center m-3 p-3">
+                    <FaGooglePlay className="w-1/5 h-fit text-white m-3"/>
+                    <span className="font-bold text-white mb-1">Yayınlanmış Uygulama</span>
+                    <span className="text-slate-500">3 Proje</span>
+                </div>
             </div>
         </div>
     )
