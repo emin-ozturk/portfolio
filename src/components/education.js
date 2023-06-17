5673e2a16de99856dccf7a1ec39246ce99648c66
Emin Öztürk <eminozturk963@gmail.com>
2023-06-17 11:40:25 +0300

    Farklı ekran boyutları için düzenleme

--- src/components/education.js
+++ src/components/education.js
@@ -4,7 +4,7 @@ const Education = () => {
     return (
         <div className="w-full h-full bg-bg2 px-10 md:px-24 py-10 flex flex-col items-center">
             <span className="flex flex-1 justify-center text-white text-3xl font-bold mb-10">EĞİTİM</span>
-            <div className="text-slate-500 grid grid-cols-1 sm:grid-cols gap-4 ">
+            <div className="text-slate-500 grid grid-cols-1 sm:grid-cols-2 w-full lg:w-3/4  gap-4 ">
                 <div className="flex flex-col bg-bg1 p-4 rounded-md text-left">
                     <span className="font-bold text-white">İstanbul Arel Üniversitesi</span>
                     <span>Bilgisayar Mühendisliği</span>
