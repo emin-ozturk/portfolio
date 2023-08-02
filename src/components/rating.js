380ad7504bd3711d8065923e877af5411e775429
Emin Öztürk <eminozturk963@gmail.com>
2023-08-02 17:32:37 +0300

    Beceriler sayfası tasarımı güncellendi

--- src/components/rating.js
+++ src/components/rating.js
@@ -1,23 +1,26 @@
 import React from "react";
-import { AiFillStar } from 'react-icons/ai';
 
 const Rating = (props) => {
     const { rating } = props;
 
-    const renderStar = () => {
-        const listItems = [];
-        for (let i = 0; i < 5; i++) {
-            if (i < rating) {
-                listItems.push(<AiFillStar className="w-3 h-3 text-yellow-500" key={i} />);
-            } else {
-                listItems.push(<AiFillStar className="w-3 h-3" key={i} />);
-            }
+    const renderProgress = () => {
+        switch (rating) {
+            case 1:
+                return (<div className="w-1/5 h-2 bg-green-700"></div>)
+            case 2:
+                return (<div className="w-2/5 h-2 bg-green-700"></div>)
+            case 3:
+                return (<div className="w-3/5 h-2 bg-green-700"></div>)
+            case 4:
+                return (<div className="w-4/5 h-2 bg-green-700"></div>)
+            default:
+                return (<div className="w-5/5 h-2 bg-green-700"></div>)
         }
-        return listItems;
     }
+
     return (
-        <div className="flex flex-row justify-center">
-            {renderStar()}
+        <div className="w-full h-2 bg-gray-300">
+            {renderProgress()}
         </div>
     )
 };
--- src/components/skills.js
+++ src/components/skills.js
@@ -12,47 +12,79 @@ import Rating from "./rating";
 
 const Skills = () => {
     return (
-        <div className="w-full h-full bg-bg1 px-10 md:px-24 py-10">
-            <span className="flex flex-1 justify-center text-white text-3xl font-bold mb-10">BECERİLER</span>
-            <div className="text-slate-500 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 pb-5">
+        <div className="w-full h-full bg-bg3 px-10 md:px-24 py-10">
+            <span className="flex flex-1 justify-center text-neutral-950 text-3xl font-bold mb-10">Beceriler</span>
+            <div className="text-slate-500 grid grid-cols-1 sm:grid-cols-2 w-full ">
 
-                <div className="flex flex-col flex-1 justify-center items-center bg-bg2 rounded-md p-4">
-                    <img src={nodejs} alt="" className="w-20 h-20 mx-6 mb-3 " />
-                    <Rating rating={3} />
-                </div>
-                <div className="flex flex-col flex-1 justify-center items-center bg-bg2 rounded-md p-4">
-                    <img src={mongodb} alt="" className="w-20 h-20 mx-6 mb-3" />
-                    <Rating rating={3} />
-                </div>
-                <div className="flex flex-col flex-1 justify-center items-center bg-bg2 rounded-md p-4">
-                    <img src={react} alt="" className="w-20 h-20 mx-6 mb-3" />
-                    <Rating rating={2} />
-                </div>
-                <div className="flex flex-col flex-1 justify-center items-center bg-bg2 rounded-md p-4">
-                    <img src={python} alt="" className="w-20 h-20 mx-6 mb-3" />
-                    <Rating rating={2} />
-                </div>
-                <div className="flex flex-col flex-1 justify-center items-center bg-bg2 rounded-md p-4">
-                    <img src={javascript} alt="" className="w-20 h-20 mx-6 mb-3" />
-                    <Rating rating={3} />
-                </div>
-                <div className="flex flex-col flex-1 justify-center items-center bg-bg2 rounded-md p-4">
-                    <img src={java} alt="" className="w-20 h-20 mx-6 mb-3" />
-                    <Rating rating={3} />
-                </div>
-                <div className="flex flex-col flex-1 justify-center items-center bg-bg2 rounded-md p-4">
-                    <img src={android} alt="" className="w-20 h-20 mx-6 mb-3" />
-                    <Rating rating={3} />
-                </div>
-                <div className="flex flex-col flex-1 justify-center items-center bg-bg2 rounded-md p-4">
-                    <img src={cpp} alt="" className="w-20 h-20 mx-6 mb-3" />
-                    <Rating rating={2} />
-                </div>
-                <div className="flex flex-col flex-1 justify-center items-center bg-bg2 rounded-md p-4">
-                    <img src={tailwind} alt="" className="w-20 h-20 mx-6 mb-3" />
-                    <Rating rating={3} />
+                <div className="flex flex-col text-left">
+                    <div className="flex flex-row w-full my-3">
+                        <img src={nodejs} alt="" className="w-12 h-12 mx-6 mb-3 " />
+                        <div className="w-full">
+                            <p>Node.js</p>
+                            <Rating rating={3} />
+                        </div>
+                    </div>
+                    <div className="flex flex-row w-full my-3">
+                        <img src={mongodb} alt="" className="w-12 h-12 mx-6 mb-3 " />
+                        <div className="w-full">
+                            <p>MongoDB</p>
+                            <Rating rating={3} />
+                        </div>
+                    </div>
+                    <div className="flex flex-row w-full my-3">
+                        <img src={react} alt="" className="w-12 h-12 mx-6 mb-3 " />
+                        <div className="w-full">
+                            <p>React</p>
+                            <Rating rating={2} />
+                        </div>
+                    </div>
+                    <div className="flex flex-row w-full my-3">
+                        <img src={python} alt="" className="w-12 h-12 mx-6 mb-3 " />
+                        <div className="w-full">
+                            <p>Python</p>
+                            <Rating rating={2} />
+                        </div>
+                    </div>
+                    <div className="flex flex-row w-full my-3">
+                        <img src={javascript} alt="" className="w-12 h-12 mx-6 mb-3 " />
+                        <div className="w-full">
+                            <p>Javascript</p>
+                            <Rating rating={2} />
+                        </div>
+                    </div>
                 </div>
 
+                <div className="flex flex-col text-left">
+                    <div className="flex flex-row w-full my-3">
+                        <img src={java} alt="" className="w-12 h-12 mx-6 mb-3 " />
+                        <div className="w-full">
+                            <p>Java</p>
+                            <Rating rating={3} />
+                        </div>
+                    </div>
+                    <div className="flex flex-row w-full my-3">
+                        <img src={android} alt="" className="w-12 h-12 mx-6 mb-3 " />
+                        <div className="w-full">
+                            <p>Android</p>
+                            <Rating rating={3} />
+                        </div>
+                    </div>
+                    <div className="flex flex-row w-full my-3">
+                        <img src={cpp} alt="" className="w-12 h-12 mx-6 mb-3 " />
+                        <div className="w-full">
+                            <p>C++</p>
+                            <Rating rating={2} />
+                        </div>
+                    </div>
+                    <div className="flex flex-row w-full my-3">
+                        <img src={tailwind} alt="" className="w-12 h-12 mx-6 mb-3 " />
+                        <div className="w-full">
+                            <p>tailwind</p>
+                            <Rating rating={3} />
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     )
