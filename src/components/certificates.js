b621390104ac9ae0e0aa371876f50a8b35576117
Emin Öztürk <eminozturk963@gmail.com>
2023-08-02 17:38:37 +0300

    Sertifikalar sayfası tasarımı güncellendi

--- src/components/certificates.js
+++ src/components/certificates.js
@@ -6,16 +6,16 @@ import python from '../images/python.png';
 
 const Certificates = () => {
     return (
-        <div className="w-full h-full bg-bg2 px-10 md:px-24 py-10">
-            <span className="flex flex-1 justify-center text-white text-3xl font-bold mb-10">SERTİFİKALAR</span>
+        <div className="w-full h-full bg-bg3 px-10 md:px-24 py-10">
+            <span className="flex flex-1 justify-center text-neutral-950 text-3xl font-bold mb-10">Sertifikalar</span>
             <div className="text-slate-500 text-justify grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 justify-center">
                 <a
                     href="https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=xr4t0Gn8qZ"
                     target="_blank"
                     rel="noreferrer"
                     className="flex flex-1">
-                    <div className="flex flex-col flex-1 justify-center items-center bg-bg1 rounded-md p-4">
-                        <img src={keras} alt="" className="w-20 h-20 mb-1 p-1 rounded-xl" />
+                    <div className="flex flex-col flex-1 justify-center items-center bg-white rounded-md p-4 hover:bg-gray-200">
+                        <img src={keras} alt="" className="w-16 h-16 mb-1 p-1 rounded-xl" />
                         <span className="w-28 text-s text-center">Keras İle Derin Öğrenmeye Giriş</span>
 
                     </div>
@@ -25,8 +25,8 @@ const Certificates = () => {
                     target="_blank"
                     rel="noreferrer"
                     className="flex flex-1">
-                    <div className="flex flex-col flex-1 justify-center items-center bg-bg1 rounded-md p-4">
-                        <img src={cpp} alt="" className="w-20 h-20 mb-1" />
+                    <div className="flex flex-col flex-1 justify-center items-center bg-white rounded-md p-4 hover:bg-gray-200">
+                        <img src={cpp} alt="" className="w-16 h-16 mb-1" />
                         <span className="w-28 text-s text-center">C++ ile Programlamaya Giriş</span>
                     </div>
                 </a>
@@ -35,8 +35,8 @@ const Certificates = () => {
                     target="_blank"
                     rel="noreferrer"
                     className="flex flex-1">
-                    <div className="flex flex-col flex-1 justify-center items-center bg-bg1 rounded-md p-4">
-                        <img src={designpattern} alt="" className="w-20 h-20 mb-1 p-1 rounded-xl" />
+                    <div className="flex flex-col flex-1 justify-center items-center bg-white rounded-md p-4 hover:bg-gray-200">
+                        <img src={designpattern} alt="" className="w-16 h-16 mb-1 p-1 rounded-xl" />
                         <span className="w-28 text-s text-center">Yazılım Tasarım Desenleri</span>
                     </div>
                 </a>
@@ -45,8 +45,8 @@ const Certificates = () => {
                     target="_blank"
                     rel="noreferrer"
                     className="flex flex-1">
-                    <div className="flex flex-col flex-1 justify-center items-center bg-bg1 rounded-md p-4">
-                        <img src={python} alt="" className="w-20 h-20 mb-1 p-1 rounded-xl" />
+                    <div className="flex flex-col flex-1 justify-center items-center bg-white rounded-md p-4 hover:bg-gray-200">
+                        <img src={python} alt="" className="w-16 h-16 mb-1 p-1 rounded-xl" />
                         <span className="w-28 text-s text-center">Introduction to Python</span>
                     </div>
                 </a>
@@ -55,8 +55,8 @@ const Certificates = () => {
                     target="_blank"
                     rel="noreferrer"
                     className="flex flex-1">
-                    <div className="flex flex-col flex-1 justify-center items-center bg-bg1 rounded-md p-4">
-                        <img src={python} alt="" className="w-20 h-20 mb-1 p-1 rounded-xl" />
+                    <div className="flex flex-col flex-1 justify-center items-center bg-white rounded-md p-4 hover:bg-gray-200">
+                        <img src={python} alt="" className="w-16 h-16 mb-1 p-1 rounded-xl" />
                         <span className="w-28 text-s text-center">Python ile Makine Öğrenmesi</span>
                     </div>
                 </a>
