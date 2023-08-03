549297d7e6d369e1aae87162df77210f8924942e
Emin Öztürk <eminozturk963@gmail.com>
2023-08-03 09:34:49 +0300

    Progresbar tasarımı değiştirildi

--- src/components/rating.js
+++ src/components/rating.js
@@ -2,25 +2,11 @@ import React from "react";
 
 const Rating = (props) => {
     const { rating } = props;
-
-    const renderProgress = () => {
-        switch (rating) {
-            case 1:
-                return (<div className="w-1/5 h-3 bg-yellow-500"></div>)
-            case 2:
-                return (<div className="w-2/5 h-3 bg-yellow-500"></div>)
-            case 3:
-                return (<div className="w-3/5 h-3 bg-yellow-500"></div>)
-            case 4:
-                return (<div className="w-4/5 h-3 bg-yellow-500"></div>)
-            default:
-                return (<div className="w-5/5 h-3 bg-yellow-500"></div>)
-        }
-    }
-
+    const width = "w-" + rating + "/5"
+  
     return (
         <div className="w-full h-3 bg-gray-200 mt-1">
-            {renderProgress()}
+            <div className = { width + " h-3 bg-gradient-to-r from-cyan-500 to-blue-300" }></div>
         </div>
     )
 };
