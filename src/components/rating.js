9ac29776f9b78d440e1514ed1ced8ea1e6e279de
Emin Öztürk <eminozturk963@gmail.com>
2023-08-05 09:32:55 +0300

    Beceriler tararımı güncellendi

--- src/components/rating.js
+++ src/components/rating.js
@@ -2,11 +2,16 @@ import React from "react";
 
 const Rating = (props) => {
     const { rating } = props;
-    const width = ((rating * 100) / 5) + '%'
+    const width = rating + '%'
   
     return (
-        <div className="w-full h-3 bg-gray-200 mt-1">
-            <div className = "h-3 bg-gradient-to-r from-cyan-600 to-blue-300" style={{width: width}}></div>
+        <div className=" flex flex-row items-center">
+            <div className="w-full h-1.5 bg-gray-300 mt-1 rounded-lg">
+                <div className = "h-1.5 bg-gradient-to-r from-cyan-300 to-blue-600 rounded-lg" style={{width: width}}></div>
+            </div>
+            <span className="ml-4 text-slate-950 font-bold">
+                { width }
+            </span>
         </div>
     )
 };
--- src/components/skills.js
+++ src/components/skills.js
@@ -14,73 +14,64 @@ const Skills = () => {
     return (
         <div className="w-full h-full bg-bg3 px-10 md:px-24 py-10">
             <span className="flex flex-1 justify-center text-neutral-950 text-3xl font-bold mb-10">Beceriler</span>
-            <div className="text-slate-500 grid grid-cols-1 sm:grid-cols-2 w-full font-serif">
+            <div className="text-slate-500 grid grid-cols-1 sm:grid-cols-2 w-full">
 
                 <div className="flex flex-col text-left sm:mr-5">
-                    <div className="flex flex-row w-full my-3">
-                        <img src={nodejs} alt="" className="w-12 h-12 mr-6 mb-3 " />
+                    <div className="flex flex-row w-full my-5 items-center">
+                        <img src={nodejs} alt="" className="w-10 h-10 mr-6" />
                         <div className="w-full">
-                            <p>Node.js</p>
-                            <Rating rating={3} />
+                            <Rating rating={70} />
                         </div>
                     </div>
-                    <div className="flex flex-row w-full my-3">
-                        <img src={mongodb} alt="" className="w-12 h-12 mr-6 mb-3 " />
+                    <div className="flex flex-row w-full my-5 items-center">
+                        <img src={mongodb} alt="" className="w-10 h-10 mr-6" />
                         <div className="w-full">
-                            <p>MongoDB</p>
-                            <Rating rating={3} />
+                            <Rating rating={55} />
                         </div>
                     </div>
-                    <div className="flex flex-row w-full my-3">
-                        <img src={react} alt="" className="w-12 h-12 mr-6 mb-3 " />
+                    <div className="flex flex-row w-full my-5 items-center">
+                        <img src={react} alt="" className="w-10 h-10 mr-6" />
                         <div className="w-full">
-                            <p>React</p>
-                            <Rating rating={2} />
+                            <Rating rating={45} />
                         </div>
                     </div>
-                    <div className="flex flex-row w-full my-3">
-                        <img src={python} alt="" className="w-12 h-12 mr-6 mb-3 " />
+                    <div className="flex flex-row w-full my-5 items-center">
+                        <img src={python} alt="" className="w-10 h-10 mr-6" />
                         <div className="w-full">
-                            <p>Python</p>
-                            <Rating rating={2} />
+                            <Rating rating={50} />
                         </div>
                     </div>
-                    <div className="flex flex-row w-full my-3">
-                        <img src={javascript} alt="" className="w-12 h-12 mr-6 mb-3 " />
+                    <div className="flex flex-row w-full my-5 items-center">
+                        <img src={javascript} alt="" className="w-10 h-10 mr-6" />
                         <div className="w-full">
-                            <p>Javascript</p>
-                            <Rating rating={2} />
+                            <Rating rating={60} />
                         </div>
                     </div>
                 </div>
 
                 <div className="flex flex-col text-left sm:ml-5">
-                    <div className="flex flex-row w-full my-3">
-                        <img src={java} alt="" className="w-12 h-12 mr-6 mb-3 " />
+                    <div className="flex flex-row w-full my-5 items-center">
+                        <img src={java} alt="" className="w-10 h-10 mr-6" />
                         <div className="w-full">
-                            <p>Java</p>
-                            <Rating rating={3} />
+                            <Rating rating={60} />
                         </div>
                     </div>
-                    <div className="flex flex-row w-full my-3">
-                        <img src={android} alt="" className="w-12 h-12 mr-6 mb-3 " />
+                    <div className="flex flex-row w-full my-5 items-center">
+                        <img src={android} alt="" className="w-10 h-10 mr-6" />
                         <div className="w-full">
-                            <p>Android</p>
-                            <Rating rating={3} />
+                            <Rating rating={60} />
                         </div>
                     </div>
-                    <div className="flex flex-row w-full my-3">
-                        <img src={cpp} alt="" className="w-12 h-12 mr-6 mb-3 " />
+                    <div className="flex flex-row w-full my-5 items-center">
+                        <img src={cpp} alt="" className="w-10 h-10 mr-6" />
                         <div className="w-full">
-                            <p>C++</p>
-                            <Rating rating={2} />
+                            <Rating rating={45} />
                         </div>
                     </div>
-                    <div className="flex flex-row w-full my-3">
-                        <img src={tailwind} alt="" className="w-12 h-12 mr-6 mb-3 " />
+                    <div className="flex flex-row w-full my-5 items-center">
+                        <img src={tailwind} alt="" className="w-10 h-10 mr-6" />
                         <div className="w-full">
-                            <p>Tailwind</p>
-                            <Rating rating={3} />
+                            <Rating rating={60} />
                         </div>
                     </div>
                     
